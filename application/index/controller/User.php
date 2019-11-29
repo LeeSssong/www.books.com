1a6e7ae41dc6f649926426e02ec2dfc085f7772c
LeeSssong <940310446@qq.com>
2019-11-29 22:47:24 +0800

    新增验证功能

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -3,10 +3,22 @@
 
 namespace app\index\controller;
 
+
+use think\Request;
+
 class User extends Base
 {
     public function login(){
         $this->alreadyLogin();
         return $this->view->fetch();
+    }
+
+    public function checkLogin(Request $request)
+    {
+        $status = 0;
+        $result = '验证失败';
+        $data = $request->param();
+
+        return ['status'=>$status, 'message'=>$result, 'data'=>$data];
     }
 }
