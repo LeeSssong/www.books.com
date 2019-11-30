9042b3885fc03e52b8b0b5e1d9a9de17fd251e7e
LeeSssong <940310446@qq.com>
2019-12-01 00:06:31 +0800

    新增验证功能

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -4,7 +4,9 @@
 namespace app\index\controller;
 
 
+use app\index\model\User as UserModel;
 use think\Request;
+use think\Session;
 
 class User extends Base
 {
@@ -18,6 +20,41 @@ class User extends Base
         $status = 0;
         $result = '验证失败';
         $data = $request->param();
+
+        //验证规则
+        $rule = [
+            'name|姓名' => 'require',
+            'password|密码'=>'require',
+        ];
+
+        //验证数据 $this->validate($data, $rule, $msg)
+        $result = $this -> validate($data, $rule);
+
+        //通过验证后,进行数据表查询
+        //此处必须全等===才可以,因为验证不通过,$result保存错误信息字符串,返回非零
+        if (true === $result) {
+            //查询条件
+            $map = [
+                'name' => $data['name'],
+                'password' => md5($data['password'])
+            ];
+
+            //数据表查询,返回模型对象
+            $user = UserModel::get($map);
+            if (null === $user) {
+                $result = '没有该用户,请检查';
+            } else {
+                $status = 1;
+                $result = '验证通过,点击[确定]后进入后台';
+
+                //创建2个session,用来检测用户登陆状态和防止重复登陆
+                Session::set('user_id', $user -> id);
+                Session::set('user_info', $user -> getData());
+
+                //更新用户登录次数:自增1
+                $user -> setInc('login_count');
+            }
+        }
 
         return ['status'=>$status, 'message'=>$result, 'data'=>$data];
     }
