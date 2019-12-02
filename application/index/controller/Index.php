6d6268fcd5f369dfc9de86164b35cff99b73f236
LeeSssong <940310446@qq.com>
2019-12-02 19:53:44 +0800

    当session存在时，直接跳转到对应的主页

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -6,6 +6,11 @@ class Index extends Base
     public function index()
     {
         $this->isLogin();
+        $this->redirect('User/index');
+    }
+
+    public function userIndex()
+    {
         return $this->view->fetch();
     }
 }
