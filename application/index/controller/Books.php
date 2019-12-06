beb0f613d91f783d946b5bcc458b890a8d34d9b7
LeeSssong <940310446@qq.com>
2019-12-06 19:47:00 +0800

    点击名字弹出模态框显示详情信息

--- application/index/controller/Books.php
+++ application/index/controller/Books.php
@@ -26,46 +26,20 @@ class Books extends Base
     public function details(Request $request)
     {
         $book_name = $request->param('name');
-
+        $message = '';
         //获取书本名字对应的数据，并返回对应模型
-        $result = InfoModel::get($book_name);
-        $author = InfoModel::get(['name' => $book_name]);
+        $result = InfoModel::get(['name' => $book_name]);
 
         $name = $book_name;
-        $author = InfoModel::get(['name' => $book_name]);
-        $press = InfoModel::get(['name' => $book_name]);
-        $press_time = InfoModel::get(['name' => $book_name]);
-        $price = InfoModel::get(['name' => $book_name]);
-        $ISBN = InfoModel::get(['name' => $book_name]);
-        $desc = InfoModel::get(['name' => $book_name]);
-//        $data = [
-//            'name'=>(string)$name,
-//            'author'=>(string)$author,
-//            'press'=>(string)$press,
-//            'press_time'=>(string)$press_time,
-//            'price'=>(string)$price,
-//            'ISBN'=>(string)$ISBN,
-//            'desc'=>(string)$desc
-//            ];
-        return ['name'=>(string)$name,'author'=>(string)$author,'author'=>(string)$author,
-            'press'=>(string)$press,'press_time'=>(string)$press_time,'price'=>(string)$price,
-            'ISBN'=>(string)$ISBN,'desc'=>(string)$desc];
-//        return json(['data'=>$data]);
-//        $this->ajaxReturn($result);
+        $author = $result->author;
+        $press = $result->press;
+        $press_time = $result->press_time;
+        $price = $result->price;
+        $ISBN = $result->ISBN;
+        $desc = $result->desc;
 
-//
-//        //根据模型取出user_id字段对应值
-//        $book_user_id = $result->getData('user_id');
-//
-//        //根据书本的user_id取出user表中对应用户的id
-//        $user_id = UserModel::get(['id' => $book_user_id]);
-//
-//        //根据用户id取出用户名字
-//        $user_name = $user_id->getData('name');
-//
-//        $this->view->assign('user_name',$user_name);
-//        $this->view->assign('book_info',$result);
-
-//        return ['data'=>$result];
+        return ['name'=>$name,'author'=>$author,'author'=>$author,
+            'press'=>$press,'press_time'=>$press_time,'price'=>$price,
+            'ISBN'=>$ISBN,'desc'=>$desc];
     }
 }
